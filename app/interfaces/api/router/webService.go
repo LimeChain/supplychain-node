2053b719238edb231194bd9108476a4753a662b9
Valentin Staykov <v.staykov@razorlabs.com>
2021-03-18 12:15:52 +0200

    added test web service

--- app/interfaces/api/router/webService.go
+++ app/interfaces/api/router/webService.go
@@ -1,8 +1,9 @@
 package router
 
 import (
-	"net/http"
-
+    "bytes"
+    "encoding/json"
+    "net/http"
 	"github.com/Limechain/HCS-Integration-Node/app/business/apiservices"
 	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
@@ -10,8 +11,28 @@ import (
 
 func getWebServiceTest(webService *apiservices.WebService) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("wegwegweg")
-	}
+		
+		values := map[string]string{"ac": "a", "occupation": "gardener"}
+		json_data, err := json.Marshal(values)
+	
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		resp, err := http.Post("http://hedera-web-service:11136/api/shipment", "application/json",
+			bytes.NewBuffer(json_data))
+	
+		if err != nil {
+			log.Println("Error:")
+			log.Fatal(err)
+		}
+	
+		var res map[string]interface{}
+	
+		json.NewDecoder(resp.Body).Decode(&res)
+		
+		log.Println("Response:")
+		log.Println(res["json"])	}
 }
 
 func NewWebServiceRouter(webService *apiservices.WebService) http.Handler {
